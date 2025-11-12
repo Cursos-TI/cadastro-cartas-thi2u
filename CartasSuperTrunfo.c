0a013ac20703123d0ce4c7a29318f6edc37f19f9
thi2u <thiago.jsp02@gmail.com>
2025-11-12 17:20:18 +0000

    Problema no código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,21 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado;
+  int num_cod;
+  char codigo_estado[4];
 
+  printf("------  Inicializando Sistema: Cadastro de Cartas   ------\n\n");
+  printf("Bem-vindo ao Jogo Super Trunfo Cidades.\nO mesmo consiste em uma batalha de atributos entre cidades registrados em uma carta.\n");
+  printf("Os atributos definidos analisados são:\n- População\n- Área\n- PIB\n- Número de pontos turísticos\n\nEntendida às regras, vamos ao Cadastro de Cartas.\n\n");
+  printf("Vamos começar pelo código do estado. Os códigos são compostos por uma letra e um inteiro.\nOs estados se dividem em 8, listados alfabeticamente de A a H.\n Digite a letra do Estado: ");
+  scanf("%c", &estado);
+  printf("Perfeito! Agora digite o inteiro. Escolha um número de 1 a 4: ");
+  scanf("%d", &num_cod);
+  codigo_estado = "estado + num_cod;";
+  printf("Resposta: %ls", codigo_estado);
   // Área para entrada de dados
-
   // Área para exibição dos dados da cidade
 
-return 0;
-} 
+  return 0;
+  } 
